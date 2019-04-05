f522f5e5c1a1ccfe229bb15935eb8f14c2a317e2
kuniaki-yokozawa <cbs5058@icloud.com>
2019-04-05 14:14:29 +0900

    workspace

---
+++ pictgram/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, length: { maximum: 255}
+  validates :password, presence: true
+
+  has_secure_password
+
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+end
